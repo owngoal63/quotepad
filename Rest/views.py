ff580666efbd3b1b58c0d49b12645df10154f0ac
owngoal63 <gordonlindsay@virginmedia.com>
2019-10-17 22:43:38 +0100

    Further update for workload_cost

--- Rest/views.py
+++ Rest/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework import viewsets, permissions
 from django.contrib.auth.models import User
 from boilerinfo.models import ProductPrice, Document, Profile
 from .serializers import ProductPriceSerializer, DocumentSerializer, ProfileSerializer, UserSerializer
+
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 
 class UserView(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -23,5 +28,30 @@ class DocumentView(viewsets.ModelViewSet):
     queryset = Document.objects.all()
     serializer_class = DocumentSerializer
     #permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+class ProfileList(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'Rest/profile_list.html'
+
+    def get(self, request):
+        queryset = Profile.objects.all()
+        return Response({'profiles': queryset})
+
+class ProfileDetail(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'Rest/profile_detail.html'
+
+    def get(self, request, pk):
+        profile = get_object_or_404(Profile, pk=pk)
+        serializer = ProfileSerializer(profile)
+        return Response({'serializer': serializer, 'profile': profile})
+
+    def post(self, request, pk):
+        profile = get_object_or_404(Profile, pk=pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if not serializer.is_valid():
+            return Response({'serializer': serializer, 'profile': profile})
+        serializer.save()
+        return redirect('profile-list')        
 
 
